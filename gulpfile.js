6a02a317dd1fd7f26829e21575fec3408313e0d5
Vincenzo Pappano <vincenzo.pappano@gmail.com>
2017-08-18 21:23:26 -0400

    Add Browserify processing

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp   = require('gulp');
 var gutil  = require('gulp-util');
 var coffee = require('gulp-coffee');
 var concat = require('gulp-concat');
+var browserify = require('gulp-browserify');
 
 gulp.task('log', function(){
   gutil.log('gulp-util test');
@@ -25,5 +26,6 @@ gulp.task('coffee', function() {
 gulp.task('js', function() {
 	gulp.src(jsSources)
 	    .pipe(concat('script.js'))
+	    .pipe(browserify())
 	    .pipe(gulp.dest('builds/development/js'))
 })
